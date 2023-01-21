482b5dc63737b4a556619ff20da39e90a15a090d
wintyo <wintyo1024@gmail.com>
2023-01-21 11:08:04 +0900

    npmrcを生成するスクリプトを作成

---
+++ scripts/generate-npmrc.sh
@@ -0,0 +1,8 @@
+source .env
+
+if [ -z $GITHUB_PKG_TOKEN ]; then
+  echo "GITHUB_PKG_TOKENの環境変数がありません"
+  exit -1
+fi
+
+sed -e "s/\${GITHUB_PKG_TOKEN}/$GITHUB_PKG_TOKEN/" ./scripts/.npmrc.template > .npmrc
---
+++ setup.sh
@@ -0,0 +1,1 @@
+./scripts/generate-npmrc.sh
